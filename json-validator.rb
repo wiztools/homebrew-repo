001710b9cf28d5506f94383588736b8742338f57
Subhash Chandran <subwiz@gmail.com>
2018-12-06 22:25:00 +0530

    Updated to v0.2.2 of json-validator

--- json-validator.rb
+++ json-validator.rb
@@ -1,8 +1,8 @@
 class JsonValidator < Formula
   desc "Cli Json validator and formattor"
   homepage "https://github.com/wiztools/json-validator/"
-  url "https://github.com/wiztools/json-validator/releases/download/0.2.1/json-validator-fat-0.2.1.jar"
-  sha256 "1801eaeb11d686048be1e8d2292a080e9682661ae664ad3a07716be533ab9aee"
+  url "https://github.com/wiztools/json-validator/releases/download/0.2.2/json-validator-fat-0.2.2.jar"
+  sha256 "88a76342a1c46dc2aef785ab57a207711ab6aaada42f97146cda65f30aa3ad10"
 
   depends_on :java => "1.8"
 
