3d68ed1eab28de3bc27472f95e867ef8a7451a14
Subhash Chandran <subwiz@gmail.com>
2018-12-15 23:11:36 +0530

    Adding cask restclient

---
+++ restclient.rb
@@ -0,0 +1,11 @@
+cask 'restclient' do
+  version '3.7.1'
+  sha256 '1a72cbab1ce50b354304b172b65aeebb7b8289b878acf78ad3ed293fdb8ecfab'
+
+  url 'https://github.com/wiztools/rest-client/releases/download/3.7.1/restclient-ui-3.7.1.dmg'
+  appcast 'https://github.com/wiztools/rest-client/releases.atom'
+  name 'restclient'
+  homepage 'https://github.com/wiztools/rest-client/'
+
+  app 'RESTClient.app'
+end
