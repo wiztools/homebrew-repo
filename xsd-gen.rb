e059611c0e43b65a910fadeb71ee5fb420dc8299
Subhash Chandran <subwiz@gmail.com>
2024-12-12 08:06:14 +0530

    xsd-gen 0.3.0 version released

--- xsd-gen.rb
+++ xsd-gen.rb
@@ -1,15 +1,15 @@
 class XsdGen < Formula
   desc "Java cli tool for generating XSD from an XML"
   homepage "https://github.com/wiztools/xsd-gen/"
-  url "https://github.com/wiztools/xsd-gen/releases/download/0.2.4/xsd-gen-fat-0.2.4.jar"
-  sha256 "89dabd9392a509c4649beebb3675004afd1ac6afa23737cbdd8b7d7d7241954f"
+  url "https://github.com/wiztools/xsd-gen/releases/download/v0.3.0/xsd-gen-0.3.0-fat.jar"
+  sha256 "4fbcb9fa5c1f60f4fcf1e3d0628aa9fe2847b55ba1bf4d11974c6bad3b703ebb"
 
   bottle :unneeded
-  depends_on :java => "1.8"
+  # depends_on :java => "1.8"
 
   def install
-    libexec.install "xsd-gen-fat-#{version}.jar"
-    bin.write_jar_script libexec/"xsd-gen-fat-#{version}.jar", "xsd-gen"
+    libexec.install "xsd-gen-#{version}-fat.jar"
+    bin.write_jar_script libexec/"xsd-gen-#{version}-fat.jar", "xsd-gen"
   end
 
   test do
